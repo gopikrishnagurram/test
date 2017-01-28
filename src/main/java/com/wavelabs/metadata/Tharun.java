d8ad08ccca46ce357844e8164584bcc586c00037
Bairoju.it05 <tharun.it05@gmail.com>
2017-01-28 19:11:53 +0530

    dependecyt i quit

--- src/main/java/com/wavelabs/metadata/Tharun.java
+++ src/main/java/com/wavelabs/metadata/Tharun.java
@@ -2,6 +2,8 @@ package com.wavelabs.metadata;
 
 public class Tharun {
 	public static void main(String[] args) {
-		System.out.println("hello git hub");
+		System.out.println("java java java java");
+		System.out.println("java java java");
+		System.out.println("gopi tharun");
 	}
 }
